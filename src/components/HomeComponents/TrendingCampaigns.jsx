42a8228dfb06739b818b287ee4c4e0e4f4883420
Shiny0805 <johnwadelinson@gmail.com>
2024-03-01 15:05:08 +0100

    update

--- src/components/HomeComponents/TrendingCampaigns.jsx
+++ src/components/HomeComponents/TrendingCampaigns.jsx
@@ -1,21 +1,29 @@
 import React from "react";
 
+const TrendingCampaignsData = [
+    {
+        image: '',
+        name: 'OpenStamp',
+        description: 'OpenStamp x Ta'
+    }
+]
+
 export default function TrendingCampaigns() {
 
   return (
     <div className="container m-auto">
         <div class="flex justify-between items-center py-10">
-            <div className="text-3xl">
+            <div className="text-3xl font-bold">
                 Trending Campaigns
             </div>
 
-            <div>
+            <div class='text-xl cursor-pointer font-light text-white hover:text-slate-400'>
                 View All
             </div>
         </div>
         
         <div>
-            Trending
+            
         </div>
 
     </div>
