0b69fbd0b1bee79c5d61ede6113d79b39bd84428
Shiny0805 <johnwadelinson@gmail.com>
2024-03-01 07:36:15 +0100

    update

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -26,14 +26,49 @@ export default function Header() {
 
             <div className="hidden lg:flex absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
               <ul className="nav_list">
-                {routes.map((link, key) => (
+              {routes.map((link, key) => (
                   <li
                     className={`list_item ${
                       currentUrl === link.url ? "active" : ""
                     }`}
                     key={key}
                   >
-                    <a href={link.url}>{link.name}</a>
+                    {link.dropdownItems ? (
+                      <div>
+                        <button
+                          id="dropdownHoverButton" 
+                          data-dropdown-toggle="dropdownHover" 
+                          data-dropdown-trigger="hover"
+                          class="text-white inline-flex items-center"
+                        >
+                          {link.name}
+                          <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+                          </svg>  
+                        </button>
+
+                        <div id="dropdownHover" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+                          <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownHoverButton">
+                            {link.dropdownItems.map((dropdownItem, index) => (
+                              <li class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                                <a
+                                  key={index}
+                                  href={dropdownItem.url}
+                                  className={`dropdown-item ${
+                                    currentUrl === dropdownItem.url ? "active" : ""
+                                  }`}
+                                >
+                                  {dropdownItem.name}
+                                </a>
+                              </li>
+                          ))}
+                          </ul>
+                        </div>
+                        
+                      </div>
+                    ) : (
+                      <a href={link.url}>{link.name}</a>
+                    )}
                     <img src="/assets/hover.png" alt="" />
                   </li>
                 ))}
