d917027874e5399e0c9a7777d8aa5ffa7171dafd
Shiny0805 <johnwadelinson@gmail.com>
2024-03-04 20:17:49 +0100

    update

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,16 +1,59 @@
 import React from "react";
-  import DDawgsBanner from "components/DDawgsComponents/Banner";
-  import DDawgsImage from "components/DDawgsComponents/Image";
-  import DDawgsBasicGrid from "components/DDawgsComponents/BasicGrid";
+import { Disclosure } from "@headlessui/react";
+import { ChevronUpIcon } from "uikit";
   
   export default function AboutUs() {
 
     return (
       <div className="flex justify-center w-full md:max-w-7xl mt-12">
         <div className="container m-3">
-          <DDawgsBanner />
-          <DDawgsImage />
-          <DDawgsBasicGrid />
+          About Us
+        </div>
+        <div>
+          <div class="w-full px-4 pt-16">
+            <div class='mx-auto w-full max-w-md rounded-2xl bg-white p-2'>
+              <Disclosure>
+                {({ open }) => (
+                  <>
+                    <Disclosure.Button class='flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500/75'>
+                      <span>
+                        What is your refund policy?
+                      </span>
+                      <ChevronUpIcon
+                        className={`${
+                          open ? 'rotate-180 transform' : ''
+                        } h-5 w-5 text-purple-500`}
+                      />
+                    </Disclosure.Button>
+                    <Disclosure.Panel className="px-4 pb-2 pt-4 text-sm text-gray-500">
+                      If you're unhappy with your purchase for any reason, email us within 90 days and we'll refund you in full, no questions asked.
+                    </Disclosure.Panel>
+                  </>
+                )}
+              </Disclosure>
+
+              <Disclosure as="div" className="mt-2">
+                {({ open }) => (
+                  <>
+                    <Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500/75">
+                      <span>
+                        Do you offer technical support?
+                      </span>
+                      <ChevronUpIcon
+                        className={`${
+                          open ? 'rotate-180 transform' : ''
+                        } h-5 w-5 text-purple-500`}
+                      />
+                    </Disclosure.Button>
+
+                    <Disclosure.Panel className="px-4 pb-2 pt-4 text-sm text-gray-500">
+                      No.
+                    </Disclosure.Panel>
+                  </>
+                )}
+              </Disclosure>
+            </div>
+          </div>
         </div>
       </div>
     );
