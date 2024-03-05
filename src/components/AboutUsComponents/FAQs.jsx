e194b3e1e22a2d14e3e2723544f7e6c1dd4beb0a
Shiny0805 <johnwadelinson@gmail.com>
2024-03-05 11:17:15 +0100

    update

--- src/components/AboutUsComponents/FAQs.jsx
+++ src/components/AboutUsComponents/FAQs.jsx
@@ -74,46 +74,31 @@ const FAQsData = [
         </div>
         <div className="flex justify-center w-full">
             <div class='w-full rounded-2xl bg-white p-2'>
-                <Disclosure>
-                    {({ open }) => (
-                    <>
-                        <Disclosure.Button class='flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-2xl font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500/75'>
-                        <span>
-                            What is your refund policy?
-                        </span>
-                        <ChevronUpIcon
-                            className={`${
-                            open ? 'rotate-180 transform' : ''
-                            } h-5 w-5 text-purple-500`}
-                        />
-                        </Disclosure.Button>
-                        <Disclosure.Panel className="px-4 pb-2 pt-4 text-sm text-gray-500">
-                        If you're unhappy with your purchase for any reason, email us within 90 days and we'll refund you in full, no questions asked.
-                        </Disclosure.Panel>
-                    </>
-                    )}
-                </Disclosure>
 
-                <Disclosure as="div" className="mt-2">
-                    {({ open }) => (
-                    <>
-                        <Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-2xl font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500/75">
-                        <span>
-                            Do you offer technical support?
-                        </span>
-                        <ChevronUpIcon
-                            className={`${
-                            open ? 'rotate-180 transform' : ''
-                            } h-5 w-5 text-purple-500`}
-                        />
-                        </Disclosure.Button>
+                {
+                    FAQsData.map((data, index)=> (
+                        <Disclosure key={index}>
+                            {({ open }) => (
+                            <>
+                                <Disclosure.Button class='flex w-full justify-between border border-black rounded-lg bg-purple-100 px-4 py-2 text-left text-2xl font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500/75'>
+                                <span>
+                                    {data.title}
+                                </span>
+                                <ChevronUpIcon
+                                    className={`${
+                                    open ? 'rotate-180 transform' : ''
+                                    } h-5 w-5 text-purple-500`}
+                                />
+                                </Disclosure.Button>
+                                <Disclosure.Panel className="px-4 pb-2 pt-4 text-sm text-gray-500">
+                                    {data.answer}
+                                </Disclosure.Panel>
+                            </>
+                            )}
+                        </Disclosure>
+                    ))
+                }
 
-                        <Disclosure.Panel className="px-4 pb-2 pt-4 text-sm text-gray-500">
-                        No.
-                        </Disclosure.Panel>
-                    </>
-                    )}
-                </Disclosure>
             </div>
         </div>
     </div>
