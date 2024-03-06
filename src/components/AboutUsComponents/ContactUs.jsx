695b2b6fa30e03ec9ef7d8e86e7cf55e573a91c5
Shiny0805 <johnwadelinson@gmail.com>
2024-03-06 07:46:58 +0100

    update

--- src/components/AboutUsComponents/ContactUs.jsx
+++ src/components/AboutUsComponents/ContactUs.jsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
   
   export default function Contactus() {
+
+    const [ name, setName ] = useState("");
+    const [ email, setEmail ] = useState('');
+    const [ message, setMessage ] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    }
 
     return (
     <div class='container m-auto'>
@@ -8,6 +16,23 @@ import React from "react";
             Contact Us
         </div>
         
+        <div class='w-3/4 m-auto'>
+            <form>
+                <div class='flex justify-between items-center'>
+                    <div class='w-1/2'>
+                        <label>First Name</label>
+                        <input class='w-full' placeholder="First Name" />
+                    </div>
+
+                    <div class='w-1/2'>
+                        <label>Last Name</label>
+                        <input class='w-full' placeholder="Last Name" />
+                    </div>
+                </div>
+                
+
+            </form>
+        </div>
     </div>
     );
   }
