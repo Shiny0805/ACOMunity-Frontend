38b03b200ae6ef36d9c1f6ca12bc395587e721a9
Shiny0805 <johnwadelinson@gmail.com>
2024-03-06 13:21:57 +0100

    update

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import FAQs from "components/AboutUsComponents/FAQs";
 import ContactUs from "components/AboutUsComponents/ContactUs";
 import Banner from "components/AboutUsComponents/Banner";
+import Vision from 'components/AboutUsComponents/Vision';
   
   export default function AboutUs() {
 
@@ -11,6 +12,7 @@ import Banner from "components/AboutUsComponents/Banner";
           About Us - title
         </div> */}
         <Banner />
+        <Vision />
         <FAQs />
         <ContactUs />
       </div>
