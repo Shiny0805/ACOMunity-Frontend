728094e4572efd7e8f44d92795790946a48e6717
Shiny0805 <johnwadelinson@gmail.com>
2024-03-06 13:50:37 +0100

    update

--- src/components/AboutUsComponents/Banner.jsx
+++ src/components/AboutUsComponents/Banner.jsx
@@ -5,18 +5,44 @@ import 'pages/style.css'
 
     return (
         <div className="aboutus_banner">
-            <div className="container m-auto flex justify-between items-center">
-                <div class='w-1/4 p-5'>
-                    <img src="./assets/AboutUs/banner_icon.png" alt="aboutus_banner_icon" />
+            <div className="container m-auto flex justify-between items-center py-5">
+                <div class='w-1/2 p-5'>
+                    <img class='rotate-12' src="./assets/AboutUs/banner_icon.png" alt="aboutus_banner_icon" />
                 </div>
-                <div class='w-2/3'>
-                    <div class='text-4xl text-center p-10'>
-                        Why ACOMUnity?
+                <div class='w-1/2 p-5'>
+                    <div>
+                        <div class='text-3xl text-center p-5'>
+                            Why ACOMUnity?
+                        </div>
+                        <div className="text-xl py-5">
+                            Envision a world where work is borderless, payments are transparent, and community collaborate drives success.
+                            ACOMUnity is not just a platform; it's a movement redefining the future of work.
+                        </div>
                     </div>
-                    <div className="text-2xl py-10">
-                        Envision a world where work is borderless, payments are transparent, and community collaborate drives success.
-                        ACOMUnity is not just a platform; it's a movement redefining the future of work.
+
+                    <div>
+                        <div class='text-3xl text-center'>
+                            Our Vision :
+                        </div>
+
+                        <div class='text-xl py-5'>
+                            ACOMUnity envisions a future where the global workforce operates in a decentralized, transparent, and collaborative ecosystem.
+                            AGEMS Token is our catalyst for change, driving innovation and empowerment.
+
+                            <p class='text-2xl py-3'>
+                                Welcome to ACOMUnity.com - Where Innovation Meets Opportunity!
+                            </p>
+                        </div>
                     </div>
+                </div>
+            </div>
+
+            <div class='container m-auto'>
+                <div class='text-3xl text-center'>
+                    Empowerment through Technology:
+                </div>
+                <div class='text-xl py-5'>
+                    At the core of ACOMUnity.com lies a symphony of cutting-edge technologies. Seamlessly integrating TOKEN's, SWAP mechanisms, NFT's, immersive GAMES, and groundbreaking AI Integration, we pave the way for a revolution in freelancing and outsourcing.
                 </div>
             </div>
         </div>
--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import FAQs from "components/AboutUsComponents/FAQs";
 import ContactUs from "components/AboutUsComponents/ContactUs";
 import Banner from "components/AboutUsComponents/Banner";
-import Vision from 'components/AboutUsComponents/Vision';
   
   export default function AboutUs() {
 
@@ -12,7 +11,6 @@ import Vision from 'components/AboutUsComponents/Vision';
           About Us - title
         </div> */}
         <Banner />
-        <Vision />
         <FAQs />
         <ContactUs />
       </div>
